c8ee3af163ee02edd4723e5e05fd11c2735a5091
thinhphungdev <thinhphung.contact@gmail.com>
2021-09-27 20:54:35 +0700

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const getWeatherData = () => {
     const location = search.value;
     message2.textContent = 'Loading...' 
     
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then(res => res.json())
     .then(data => {
         if (data.error) {
